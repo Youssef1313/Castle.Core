207f9f73b20fae50c03802547df08651ed32bb0c
Youssef1313 <youssefvictor00@gmail.com>
2026-05-02 09:46:05 +0200

    Use ConcurrentDictionary in ModuleScope

--- src/Castle.Core/DynamicProxy/Contributors/ClassProxyTargetContributor.cs
+++ src/Castle.Core/DynamicProxy/Contributors/ClassProxyTargetContributor.cs
@@ -176,7 +176,7 @@ namespace Castle.DynamicProxy.Contributors
 				GetCacheKeyTypes(method),
 				null);
 
-			return scope.TypeCache.GetOrAddWithoutTakingLock(key, _ =>
+			return scope.TypeCache.GetOrAdd(key, _ =>
 				new DelegateTypeGenerator(method, targetType)
 				.Generate(@class, namingScope)
 				.BuildType());
